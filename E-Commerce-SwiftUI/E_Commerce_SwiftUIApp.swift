4a1a15764aff3d311a71fccb81dcc7eae8272ab0
hakkicansengonul <coder.9734@gmail.com>
2022-06-12 10:33:49 +0300

    Initial Commit

---
+++ E-Commerce-SwiftUI/E_Commerce_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_SwiftUIApp.swift
+//  E-Commerce-SwiftUI
+//
+//  Created by hakkı can şengönül on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
